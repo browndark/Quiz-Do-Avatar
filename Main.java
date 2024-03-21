2407f7c92bec73a28ec0ddf318e9e20b24875bb5
browndark <163913166+browndark@users.noreply.github.com>
2024-03-21 18:31:26 -0300

    Add files via upload
    Bruno custodio de castro
    15 perguntas do quiz

---
+++ Main.java
@@ -0,0 +1,16 @@
+//Faculdade Unifan
+//Bruno Custodio de Castro Silva
+//Segundo periodo
+public class Main {
+    public static void main(String[] args) {
+        int numeroAcertos = 0;
+        System.out.println("===== Quiz: Avatar - A Lenda de Aang =====");
+        numeroAcertos += grupo2();
+        System.out.println("Fim do Quiz! Parabéns... De 15 perguntas, você acertou: " + numeroAcertos);
+    }
+
+    public static int grupo2() {
+        int acertosGrupo2 = 3;
+        return acertosGrupo2;
+    }
+}
